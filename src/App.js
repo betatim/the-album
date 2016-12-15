f9dda719dcd372e9c620a2f25dfb1140a610600f
Tim Head <betatim@gmail.com>
2016-12-15 23:50:47 +0100

    Mixing all the material design libraries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
-import {GridList, GridTile} from 'material-ui/GridList';
+import { GridList, GridTile } from 'material-ui/GridList';
+import Paper from 'material-ui/Paper';
 
 import './App.css';
 
@@ -52,63 +53,81 @@ class AlbumApp extends Component {
     const focusItem = this.state.items.find(
       e => (e.id === this.state.focussed));
     return (
-      <div>
-        <h3>TODO</h3>
-        <form onSubmit={this.handleSubmit}>
-          <input onChange={this.handleChange} value={this.state.text} />
-          <button>{'Add #' + (this.state.items.length + 1)}</button>
-        </form>
-        {this.state.focussed ?
-          <TodoItem
-            item={focusItem}
-            onClick={this.handleUnFocus}
-          />
-          :
-          <TodoList
-            items={this.state.items}
-            handleFocus={this.handleFocusItem}
-          />
-        }
+      <div style={{ margin: 'auto', maxWidth: '900px' }}>
+        <div>
+          <h3>TODO</h3>
+          <form onSubmit={this.handleSubmit}>
+            <input onChange={this.handleChange} value={this.state.text} />
+            <button>{'Add #' + (this.state.items.length + 1)}</button>
+          </form>
+          <hr />
+          {this.state.focussed ?
+            <FocussedItem
+              item={focusItem}
+              onClick={this.handleUnFocus}
+            />
+            :
+            <TodoList
+              items={this.state.items}
+              handleFocus={this.handleFocusItem}
+            />
+          }
+        </div>
       </div>
     );
   }
 }
 
-const styles = {
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-  },
-  gridList: {
-    width: 800,
-    overflowY: 'auto',
-  },
-};
-const TodoList = props => (
-  <GridList
-    cols={4}
-    // cellHeight={180}
-    style={styles.gridList}
-  >
-    {props.items.map(item => (
-      <GridTile
-        key={item.id}
-        title={item.text}
-        onClick={() => props.handleFocus(item.id)}
-      >
-        <img
-          alt={item.text}
-          src="http://indianapublicmedia.org/eartheats/files/2009/12/bicycle.jpg"
-        />
-      </GridTile>
-    ))}
-  </GridList>
-);
 
-const TodoItem = (props) => {
+const TodoList = props => {
+  const styles = {
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+    },
+    gridList: {
+      overflowY: 'auto',
+    },
+  };
+  return (
+    <GridList
+      cols={4}
+      style={styles.gridList}
+    >
+      {props.items.map(item => (
+        <GridTile
+          key={item.id}
+          title={item.text}
+          onClick={() => props.handleFocus(item.id)}
+        >
+          <img
+            alt={item.text}
+            src="http://indianapublicmedia.org/eartheats/files/2009/12/bicycle.jpg"
+          />
+        </GridTile>
+      ))}
+    </GridList>);
+};
+
+const FocussedItem = (props) => {
+  const style = {
+    margin: 20,
+    textAlign: 'left',
+    //display: 'inline-block',
+    padding: '0 10px',
+  };
   const item = props.item;
-  return (<li onClick={() => props.onClick()}>{item.text}</li>);
+  return (
+    <Paper style={style} zDepth={1} onClick={() => props.onClick()}>
+      <img
+        alt={item.text}
+        src="http://indianapublicmedia.org/eartheats/files/2009/12/bicycle.jpg"
+      />
+      <h2>{item.text}</h2>
+      <p>Lorem ipsum dolor.</p>
+    </Paper>
+  );
 };
 
 
