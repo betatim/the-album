b6acc89f0b0977013970b6a453e355b1baafc101
Tim Head <betatim@gmail.com>
2016-12-16 11:27:14 +0100

    Layouting

---
+++ README.md
@@ -0,0 +1,3 @@
+# Album viewer
+
+A image viewer.
--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 
+import FlatButton from 'material-ui/FlatButton';
 import { GridList, GridTile } from 'material-ui/GridList';
-import Paper from 'material-ui/Paper';
+import { Card, CardText, CardMedia, CardTitle, CardActions } from 'material-ui/Card';
+import {List, ListItem} from 'material-ui/List';
+import Divider from 'material-ui/Divider';
 
 import './App.css';
 
@@ -53,33 +56,55 @@ class AlbumApp extends Component {
     const focusItem = this.state.items.find(
       e => (e.id === this.state.focussed));
     return (
-      <div style={{ margin: 'auto', maxWidth: '900px' }}>
-        <div>
-          <h3>TODO</h3>
-          <form onSubmit={this.handleSubmit}>
-            <input onChange={this.handleChange} value={this.state.text} />
-            <button>{'Add #' + (this.state.items.length + 1)}</button>
-          </form>
-          <hr />
-          {this.state.focussed ?
-            <FocussedItem
-              item={focusItem}
-              onClick={this.handleUnFocus}
-            />
-            :
-            <TodoList
-              items={this.state.items}
-              handleFocus={this.handleFocusItem}
-            />
-          }
-        </div>
+      <div className="mdl-layout mdl-js-layout">
+        <header className="mdl-layout__header mdl-layout__header--scroll">
+          <div className="mdl-layout__header-row">
+            <span className="mdl-layout-title">The Album</span>
+          </div>
+        </header>
+        <main className="mdl-layout__content">
+          <div
+            style={{
+              padding: '8px',
+              paddingTop: '48px',
+              paddingBottom: '48px',
+              margin: 'auto',
+              maxWidth: '900px' }}
+          >
+            <form onSubmit={this.handleSubmit}>
+              <input onChange={this.handleChange} value={this.state.text} />
+              <button>{'Add #' + (this.state.items.length + 1)}</button>
+            </form>
+            <hr />
+            {this.state.focussed ?
+              <FocussedItem
+                item={focusItem}
+                onClick={this.handleUnFocus}
+              />
+              :
+              <ImageList
+                items={this.state.items}
+                handleFocus={this.handleFocusItem}
+              />
+            }
+          </div>
+        </main>
+        <footer className="mdl-mini-footer">
+          <div className="mdl-mini-footer__left-section">
+            <div className="mdl-logo">The Album</div>
+            <ul className="mdl-mini-footer__link-list">
+              <li><a href="#">Help</a></li>
+              <li><a href="#">Privacy & Terms</a></li>
+            </ul>
+          </div>
+        </footer>
       </div>
     );
   }
 }
 
 
-const TodoList = props => {
+const ImageList = props => {
   const styles = {
     root: {
       display: 'flex',
@@ -111,22 +136,27 @@ const TodoList = props => {
 };
 
 const FocussedItem = (props) => {
-  const style = {
-    margin: 20,
-    textAlign: 'left',
-    //display: 'inline-block',
-    padding: '0 10px',
-  };
   const item = props.item;
   return (
-    <Paper style={style} zDepth={1} onClick={() => props.onClick()}>
-      <img
-        alt={item.text}
-        src="http://indianapublicmedia.org/eartheats/files/2009/12/bicycle.jpg"
-      />
-      <h2>{item.text}</h2>
-      <p>Lorem ipsum dolor.</p>
-    </Paper>
+    <Card>
+      <CardMedia>
+        <img
+          alt={item.text}
+          src="http://indianapublicmedia.org/eartheats/files/2009/12/bicycle.jpg"
+        />
+      </CardMedia>
+      <CardTitle title={item.text} />
+      <CardText>
+        <List>
+          <ListItem disabled primaryText="Date" secondaryText="23 Dec 2016 12:34:56" />
+          <ListItem disabled primaryText="Location" secondaryText="Geneva, Switzerland" />
+          <ListItem disabled primaryText="Make" secondaryText="Canon" />
+        </List>
+      </CardText>
+      <CardActions className="mdl-card__actions">
+        <FlatButton primary label="Minimise" onClick={() => props.onClick()} />
+      </CardActions>
+    </Card>
   );
 };
 
